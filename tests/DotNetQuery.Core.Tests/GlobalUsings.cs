dca6583516f5a97edc64b1870405ce4d8fc4a099
psachmann <github.buffed041@passmail.net>
2026-03-28 18:08:30 +0100

    chore: code cleanup

--- tests/DotNetQuery.Core.Tests/GlobalUsings.cs
+++ tests/DotNetQuery.Core.Tests/GlobalUsings.cs
@@ -1,7 +1,5 @@
 global using System.Reactive;
-global using System.Reactive.Concurrency;
 global using System.Reactive.Linq;
-global using DotNetQuery.Core;
 global using DotNetQuery.Core.Internals;
 global using DotNetQuery.Core.Tests.Utils;
 global using Microsoft.Reactive.Testing;
