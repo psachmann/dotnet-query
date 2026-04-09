2c8357c0fe19eef4930d4b0d30163cfeb4ad3f1b
psachmann <github.buffed041@passmail.net>
2026-04-09 21:41:18 +0200

    chore: updating codecov setup

--- flake.nix
+++ flake.nix
@@ -11,7 +11,10 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
       in
       {
         devShells.default = pkgs.mkShell {
@@ -20,6 +23,7 @@
             dotnet-sdk_10
             omnisharp-roslyn
             netcoredbg
+            codecov-cli
           ];
           shellHook = ''
           '';
